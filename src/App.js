92a5af00a101958062c1a7c8aaf2d7467bb65dfc
alghifarifikri <alghi77@gmail.com>
2023-08-09 21:54:13 +0700

    remove unused code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
   return (
@@ -23,7 +22,6 @@ const App = () => {
       <Suspense fallback={loading}>
         <Routes>
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
-          <Route exact path="/500" name="Page 500" element={<Page500 />} />
           <Route path="*" name="Home" element={<DefaultLayout />} />
           <Route exact path="/login" name="Login Page" element={<Login />} />/
         </Routes>
